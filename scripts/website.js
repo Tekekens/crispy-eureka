de0a85f5b5817f4f6b062351fdfdb2f1c968d2ef
Tekekens <johnmattew313@gmail.com>
2026-03-12 07:36:35 +0800

    Add functions to toggle popup and change images

---
+++ scripts/website.js
@@ -0,0 +1,29 @@
+function button() {
+				var div =
+				document.getElementById("popup");
+				
+				if (div.style.display === "none") {
+								div.style.display = "block";
+				} else {
+								div.style.display = "none";
+				}
+}
+
+function replace1() {
+				document.getElementById('picture').src='matt7.jpg'
+};
+function replace2() {
+				document.getElementById('picture').src='matt6.png'
+};
+function replace3() {
+				document.getElementById('picture').src='matt5.jpg'
+};
+function replace4() {
+				document.getElementById('picture').src='matt3.jpg'
+};
+function replace5() {
+				document.getElementById('picture').src='matt2.jpg'
+};
+function replace6() {
+				document.getElementById('picture').src='Matt1.jpg'
+};
